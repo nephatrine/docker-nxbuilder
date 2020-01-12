7c438d9ccfadeee256efdc125696870e89d6282d
Daniel Wolf <nephatrine@gmail.com>
2020-01-11 21:01:26 -0500

    update toolchains

--- override/opt/nxb/FreeBSD_PREVIOUS.cmake
+++ override/opt/nxb/FreeBSD_PREVIOUS.cmake
@@ -9,6 +9,16 @@ set(CMAKE_C_COMPILER_TARGET ${CMAKE_SYSTEM_PROCESSOR}-unknown-freebsdVERSION)
 set(CMAKE_CXX_COMPILER clang++)
 set(CMAKE_CXX_COMPILER_TARGET ${CMAKE_SYSTEM_PROCESSOR}-unknown-freebsdVERSION)
 
+find_program(CMAKE_AR llvm-ar)
+find_program(CMAKE_RANLIB llvm-ranlib)
+find_program(CMAKE_NM llvm-nm)
+find_program(CMAKE_OBJCOPY llvm-objcopy)
+find_program(CMAKE_OBJDUMP llvm-objdump)
+find_program(CMAKE_STRIP llvm-strip)
+
+set(CMAKE_CXX_FLAGS_INIT "-stdlib=libc++")
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-fuse-ld=lld")
+
 set(CMAKE_FIND_ROOT_PATH "${CMAKE_STAGING_PREFIX};${CMAKE_SYSROOT}")
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
