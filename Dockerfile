1d7ce2c8936ce7366b8cac97e4e1ff75e8241d16
Daniel Wolf <nephatrine@gmail.com>
2020-01-14 18:34:20 -0500

    add riscv64 and remove chroot tools

--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,11 @@ LABEL maintainer="Daniel Wolf <nephatrine@gmail.com>"
 RUN echo "====== INSTALL PACKAGES ======" \
  && apt-get update -q \
  && apt-get -y -q -o Dpkg::Options::="--force-confnew" install \
-   alien mock \
+   alien debhelper \
    crossbuild-essential-arm64 crossbuild-essential-armhf crossbuild-essential-ppc64el crossbuild-essential-s390x \
-   debhelper pbuilder \
    git-buildpackage git-buildpackage-rpm mercurial-buildpackage svn-buildpackage \
+   gcc-riscv64-linux-gnu g++-riscv64-linux-gnu \
  && apt-get clean \
  && rm -rf /tmp/* /var/lib/apt/lists/* /var/tmp/*
+
+COPY override /
---
+++ override/opt/nxb/Linux_AARCH64.cmake
@@ -0,0 +1,22 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
+set(triplet aarch64-linux-gnu)
+
+list(APPEND CMAKE_PREFIX_PATH "/usr/${triplet}")
+
+set(CMAKE_C_COMPILER ${triplet}-gcc)
+set(CMAKE_C_LIBRARY_ARCHITECTURE ${triplet})
+set(CMAKE_CXX_COMPILER ${triplet}-g++)
+set(CMAKE_CXX_LIBRARY_ARCHITECTURE ${triplet})
+
+find_program(CMAKE_AR ${triplet}-ar)
+find_program(CMAKE_RANLIB ${triplet}-ranlib)
+find_program(CMAKE_NM ${triplet}-nm)
+find_program(CMAKE_OBJCOPY ${triplet}-objcopy)
+find_program(CMAKE_OBJDUMP ${triplet}-objdump)
+find_program(CMAKE_STRIP ${triplet}-strip)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
