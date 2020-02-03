bbb6b4d575eb525daf48d5060b603d8b2d209d0c
Daniel Wolf <nephatrine@gmail.com>
2020-02-03 16:41:45 -0500

    test linking native app glue

--- override/opt/nxb/src/hello/hello.cpp
+++ override/opt/nxb/src/hello/hello.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 
-int main( int, char *[] )
+#include <android/native_app_glue.h>
+
+void android_main( struct android_app * state )
 {
-	std::cout << "Hello World!" << std::endl;
-	return 0;
+	while( true )
+	{
+		struct android_poll_source * source;
+		int                          ident, events;
+
+		while( ( ident = ALooper_pollAll( -1, NULL, &events, (void **)&source ) ) >= 0 )
+		{
+			if( source != NULL ) source->process( state, source );
+			if( state->destroyRequested != 0 ) return;
+		}
+	}
 }
