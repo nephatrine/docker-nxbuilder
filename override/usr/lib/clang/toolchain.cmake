ea3ae062297c19b85e29c757db49747f0b2f8117
Daniel Wolf <nephatrine@gmail.com>
2020-07-11 14:21:45 -0400

    add compiler target to clang

--- override/usr/lib/clang/toolchain.cmake
+++ override/usr/lib/clang/toolchain.cmake
@@ -1,3 +1,8 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
+
+set(triplet "${CMAKE_SYSTEM_PROCESSOR}-linux-gnu")
+
 list(APPEND CMAKE_PREFIX_PATH "/usr/lib/llvm-${LLVM_MAJOR}")
 
 find_program(CMAKE_C_COMPILER NAMES clang)
@@ -13,7 +18,9 @@ find_program(CMAKE_OBJCOPY NAMES llvm-objcopy)
 find_program(CMAKE_READELF NAMES llvm-readelf)
 find_program(CMAKE_ADDR2LINE NAMES llvm-addr2line)
 
+set(CMAKE_C_COMPILER_TARGET ${triplet})
 set(CMAKE_C_FLAGS_INIT "--rtlib=compiler-rt")
+set(CMAKE_CXX_COMPILER_TARGET ${triplet})
 set(CMAKE_CXX_FLAGS_INIT "--rtlib=compiler-rt -stdlib=libc++")
 
 set(CMAKE_EXE_LINKER_FLAGS_INIT "-fuse-ld=lld --rtlib=compiler-rt -lunwind")
