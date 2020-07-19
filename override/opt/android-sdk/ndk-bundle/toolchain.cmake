25763023d3fa865c3fe2be7770c1d612cacf648e
Daniel Wolf <nephatrine@gmail.com>
2020-07-19 19:49:20 -0400

    manually set CMAKE_SIZEOF_VOID_P

--- override/opt/android-sdk/ndk-bundle/toolchain.cmake
+++ override/opt/android-sdk/ndk-bundle/toolchain.cmake
@@ -14,4 +14,12 @@ if(NOT DEFINED ANDROID_NATIVE_API_LEVEL)
     set(ANDROID_NATIVE_API_LEVEL 24)
 endif()
 
+if(DEFINED ANDROID_ABI)
+	if(ANDROID_ABI MATCHES "64")
+		set(CMAKE_SIZEOF_VOID_P 8)
+	else()
+		set(CMAKE_SIZEOF_VOID_P 4)
+	endif()
+endif()
+
 include(${ANDROID_NDK}/build/cmake/android.toolchain.cmake)
